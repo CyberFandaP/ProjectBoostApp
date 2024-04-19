c3f7103d6ea59039ee4c781f4b1c7000541d89e2
František Peterka <cyberfandap@seznam.cz>
2024-04-19 16:02:42 +0200

    Vytvoření hooku useLogout pro odhlášení a přidání odhlášení do Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import { useLogout } from "../hooks/useLogout"
 
 // Implementuje přecházení na různé stránky
 const Navbar = () => {
+  // Import odhlášení z hooku
+  const { logout } = useLogout()
+
+  // Funkce pro kliknutí na tlačítko a odhlášení z aplikace
+  const handleClick = () => {
+    // Funkce updatuje global state a odstraní JWT z local storage
+    logout()
+  }
+
   return (
     <header>
         <div className="container">
             <Link to="/">
-                <h1>Focus Booster</h1>
+                <h1>Workout Buddy</h1>
             </Link>
             <nav>
+              <div>
+                <button onClick={handleClick}>Log out</button>
+              </div>
               <div>
                 <Link to="/login">Login</Link>
                 <Link to="/signup">Signup</Link>
